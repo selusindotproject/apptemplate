5a07671de814d84787b1f92d61cf7bc01a91d2e2
vcsit2021 <91366489+selusindotproject@users.noreply.github.com>
2021-11-23 12:56:29 +0700

    .

--- application/config/ion_auth.php
+++ application/config/ion_auth.php
@@ -38,10 +38,10 @@ $config['database_group_name'] = '';
 | -------------------------------------------------------------------------
 | Database table names.
 */
-$config['tables']['users']           = 'users';
-$config['tables']['groups']          = 'groups';
-$config['tables']['users_groups']    = 'users_groups';
-$config['tables']['login_attempts']  = 'login_attempts';
+$config['tables']['users']           = 't85_users';
+$config['tables']['groups']          = 't86_groups';
+$config['tables']['users_groups']    = 't87_users_groups';
+$config['tables']['login_attempts']  = 't88_login_attempts';
 
 /*
  | Users table column and Group table column you want to join WITH.
